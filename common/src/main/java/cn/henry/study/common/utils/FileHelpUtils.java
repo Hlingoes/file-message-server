a709f1c8d7dd1f19f64fa8f16cd9ef32e5590b41
Hlingoes <hulin19891229@hotmail.com>
2020-05-17 22:19:44 +0800

    增加stream的io工具方法

--- common/src/main/java/cn/henry/study/common/utils/FileHelpUtils.java
+++ common/src/main/java/cn/henry/study/common/utils/FileHelpUtils.java
@@ -9,8 +9,7 @@ import org.apache.commons.lang3.StringUtils;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
-import java.io.File;
-import java.io.IOException;
+import java.io.*;
 import java.net.JarURLConnection;
 import java.net.URL;
 import java.net.URLConnection;
@@ -110,7 +109,7 @@ public class FileHelpUtils {
      * @author Hlingoes 2020/3/26
      */
     public static void writeTempFile(String rowKey, byte[] bytes) {
-        File file = FileHelpUtils.findTempleFile(rowKey);
+        File file = findTempleFile(rowKey);
         // 同一文件，多次失败，不需要重复写入
         if (file.exists()) {
             return;
@@ -161,11 +160,25 @@ public class FileHelpUtils {
         return FileUtils.getFile(dir, retryDir, fileName);
     }
 
+    /**
+     * description: 查询当前文件所属jar包的路径
+     *
+     * @param clazz
+     * @return java.io.File
+     * @author Hlingoes 2020/5/17
+     */
     public static File findHomeDir(Class clazz) {
         File source = findSource(FileHelpUtils.class);
         return findHomeDir(source).getParentFile();
     }
 
+    /**
+     * description: 查询当前文件所属jar包的路径
+     *
+     * @param source
+     * @return java.io.File
+     * @author Hlingoes 2020/5/17
+     */
     public static File findHomeDir(File source) {
         File homeDir = source;
         homeDir = (homeDir == null ? findDefaultHomeDir() : homeDir);
@@ -176,11 +189,25 @@ public class FileHelpUtils {
         return homeDir.getAbsoluteFile();
     }
 
+    /**
+     * description:返回默认的项目路径，从system中获取user.dir，路径与启动方式有关
+     *
+     * @param
+     * @return java.io.File
+     * @author Hlingoes 2020/5/17
+     */
     private static File findDefaultHomeDir() {
         String userDir = System.getProperty("user.dir");
         return new File(StringUtils.isEmpty(userDir) ? userDir : ".");
     }
 
+    /**
+     * description: 返回class文件路径
+     *
+     * @param sourceClass
+     * @return java.io.File
+     * @author Hlingoes 2020/5/17
+     */
     private static File findSource(Class<?> sourceClass) {
         try {
             ProtectionDomain domain = (sourceClass == null ? null : sourceClass.getProtectionDomain());
@@ -196,6 +223,13 @@ public class FileHelpUtils {
         }
     }
 
+    /**
+     * description: 返回class文件路径
+     *
+     * @param location
+     * @return java.io.File
+     * @author Hlingoes 2020/5/17
+     */
     private static File findSource(URL location) throws IOException {
         URLConnection connection = location.openConnection();
         if (connection instanceof JarURLConnection) {
@@ -204,6 +238,13 @@ public class FileHelpUtils {
         return new File(location.getPath());
     }
 
+    /**
+     * description: 判断是否为单元测试
+     *
+     * @param
+     * @return boolean
+     * @author Hlingoes 2020/5/17
+     */
     private static boolean isUnitTest() {
         try {
             StackTraceElement[] stackTrace = Thread.currentThread().getStackTrace();
@@ -217,6 +258,13 @@ public class FileHelpUtils {
         return false;
     }
 
+    /**
+     * description: 获取jar文件，springboot打包之后，jar包中的jar包会以"!/"开头
+     *
+     * @param jarFile
+     * @return java.io.File
+     * @author Hlingoes 2020/5/17
+     */
     private static File getRootJarFile(JarFile jarFile) {
         String name = jarFile.getName();
         int separator = name.indexOf("!/");
@@ -225,4 +273,105 @@ public class FileHelpUtils {
         }
         return new File(name);
     }
+
+    /**
+     * description: 将inputStream转为string
+     *
+     * @param inputStream
+     * @return java.lang.String
+     * @author Hlingoes 2020/5/17
+     */
+    public static String toString(InputStream inputStream) throws IOException {
+        ByteArrayOutputStream result = new ByteArrayOutputStream();
+        byte[] buffer = new byte[1024];
+        int length;
+        while ((length = inputStream.read(buffer)) != -1) {
+            result.write(buffer, 0, length);
+        }
+        return result.toString("UTF-8");
+    }
+
+    /**
+     * description: 将inputStream转为outputStream
+     *
+     * @param in
+     * @param out
+     * @return void
+     * @author Hlingoes 2020/5/17
+     */
+    public static void copy(InputStream in, OutputStream out) throws IOException {
+        byte[] buffer = new byte[1024];
+        int len;
+        while ((len = in.read(buffer)) != -1) {
+            out.write(buffer, 0, len);
+        }
+    }
+
+    /**
+     * @return a byte[] containing the information contained in the specified
+     *         InputStream.
+     * @throws java.io.IOException
+     */
+    public static byte[] getBytes(InputStream input) throws IOException {
+        ByteArrayOutputStream result = new ByteArrayOutputStream();
+        copy(input, result);
+        result.close();
+        return result.toByteArray();
+    }
+
+    public static IOException close(InputStream input) {
+        return close((Closeable) input);
+    }
+
+    /**
+     * description: 关闭流
+     *
+     * @param output
+     * @return java.io.IOException
+     * @author Hlingoes 2020/5/17
+     */
+    public static IOException close(OutputStream output) {
+        return close((Closeable) output);
+    }
+
+    /**
+     * description: 关闭reader
+     *
+     * @param input
+     * @return java.io.IOException
+     * @author Hlingoes 2020/5/17
+     */
+    public static IOException close(final Reader input) {
+        return close((Closeable) input);
+    }
+
+    /**
+     * description: 关闭writer
+     *
+     * @param output
+     * @return java.io.IOException
+     * @author Hlingoes 2020/5/17
+     */
+    public static IOException close(final Writer output) {
+        return close((Closeable) output);
+    }
+
+    /**
+     * description: 通用的close
+     *
+     * @param closeable
+     * @return java.io.IOException
+     * @author Hlingoes 2020/5/17
+     */
+    public static IOException close(final Closeable closeable) {
+        try {
+            if (closeable != null) {
+                closeable.close();
+            }
+        } catch (final IOException ioe) {
+            return ioe;
+        }
+        return null;
+    }
+
 }
