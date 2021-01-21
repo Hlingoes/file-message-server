d157646cc255fff2c876efd2c7673d6ffb5909ed
Hlingoes <hulin19891229@hotmail.com>
2021-01-21 23:21:07 +0800

    update dependence

--- common/src/test/java/cn/henry/study/common/PracticeTest.java
+++ common/src/test/java/cn/henry/study/common/PracticeTest.java
@@ -4,9 +4,13 @@ import ch.qos.logback.classic.LoggerContext;
 import ch.qos.logback.core.util.OptionHelper;
 import cn.henry.study.common.enums.LogNameEnum;
 import cn.henry.study.common.utils.LoggerUtils;
+import org.apache.commons.collections4.CollectionUtils;
 import org.junit.Test;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
+
+import java.util.Arrays;
+import java.util.List;
 
 /**
  * description:
@@ -30,6 +34,14 @@ public class PracticeTest {
         testLogger.info("testLogger#####{}####", oph);
         testLogger.info("testLogger看到这条信息就是info");
         testLogger.error("testLogger看到这条信息就是error");
+    }
+
+    @Test
+    public void collectionsTest() {
+        List<Integer> a = Arrays.asList(1, 2, 3, 3, 4, 5);
+        List<Integer> b = Arrays.asList(3, 4, 4, 5, 6, 7);
+        System.out.println(CollectionUtils.subtract(a, b));
+        System.out.println(CollectionUtils.subtract(b, a));
     }
 
 }
